4838b798a74e6bb9bf75f2aeed3a7a3f9364b4a4
overpayyy <maksym.slipchyshyn@microsoft.wsei.edu.pl>
2026-05-15 14:30:10 +0200

    Dodanie konfiguracji rejestracji aplikacji

--- MailSender.WebAPI/Controllers/ClientAppController.cs
+++ MailSender.WebAPI/Controllers/ClientAppController.cs
@@ -7,9 +7,26 @@ namespace MailSender.WebAPI.Controllers;
 [Route("client-app")]
 public class ClientAppController : ControllerBase
 {
+    private readonly IConfiguration _configuration;
+
+    public ClientAppController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     [HttpPost("register")]
     public IActionResult Register(RegisterClientRequestDto request)
     {
+        var validPasswords = _configuration.GetSection("Registration:Passwords").Get<List<string>>();
+
+        if (!validPasswords.Contains(request.Pass))
+        {
+            return StatusCode(403, new
+            {
+                error = "Invalid index-based password XX"
+            });
+        }
+
         return Ok(new
         {
             message = "Registration endpoint works",
